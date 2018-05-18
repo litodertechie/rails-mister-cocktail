2aa9abf6422246070986b026a980408edb0c1f0f
Emanuel Coen <emanuelcoen@gmail.com>
2018-05-18 11:09:35 +0200

    Exercise yesterday

---
+++ db/migrate/20180517094946_create_doses.rb
@@ -0,0 +1,10 @@
+class CreateDoses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doses do |t|
+      t.references :cocktail, foreign_key: true, index: true
+      t.references :ingredient, foreign_key: true, index: true
+      t.text :description
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data_serialized = open(url).read
+data = JSON.parse(data_serialized)
+
+data['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Sex on the beach")
+
+Dose.create(description: "A teaspoon")
+Dose.create(description: "A lot")
